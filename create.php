928bb5d07c9c3a652659c2570aa4a774c1fd879e
ArhaamAcademy <35983296+ArhaamAcademy@users.noreply.github.com>
2018-10-14 15:51:43 +0600

    Update create.php

--- create.php
+++ create.php
@@ -1,1 +1,30 @@
 
+<?php
+
+if(isset($_POST['create']))
+{
+  
+  try
+  {
+    $name             = $_POST['cat_name'];
+    $email            = $_POST['email'];
+    $contact_number   = $_POST['contact_number'];
+    $date             = date('Y-m-d');
+    $status           = 1;
+    
+    if(empty($name))
+    {
+      throw new Exception("Name cannot be empty!");
+    }
+    
+    $statement = $db->prepare("INSERT INTO users (name, email, contact_number, date, status) VALUES(?, ?, ?)");
+    $statement->execute(array($name, $email, $contact_number, $date, $status));
+    
+    $success = "The inserte successfully...!";
+  }
+  catch(Exception $e);
+  {
+    $error = $e->getMessage();
+  }
+    
+}
