2eea709880b06be06bdecd75947ebe1a8180df83
ArhaamAcademy <35983296+ArhaamAcademy@users.noreply.github.com>
2018-10-14 16:06:40 +0600

    Create update.php

---
+++ update.php
@@ -0,0 +1,43 @@
+<?php
+
+if(isset($_POST['update']))
+{
+  
+  try
+  {
+  
+    if(isset($_GET['id'])
+    {
+      $id = $_GET['id'];
+    }
+    else
+    {
+      header ();
+    }
+    
+    
+    $name             = $_POST['cat_name'];
+    $email            = $_POST['email'];
+    $contact_number   = $_POST['contact_number'];
+    $created_date      = date('Y-m-d');
+    $status           = 1;
+    
+    if(empty($name))
+    {
+      throw new Exception("Name cannot be empty!");
+    }
+    
+    $stmt = $db->prepare("UPDATE set users name = ?, email = ?, contact_number = ?, created_date = ?, ststus = ?, id = ?);
+    $stmt->execute(array($name, $email, $contact_number, $create_date, $status, $id));
+    
+    $success = "The field update successfully...!";
+  }
+  catch(Exception $e);
+  {
+    $error = $e->getMessage();
+  }
+    
+}
+
+?>
+
