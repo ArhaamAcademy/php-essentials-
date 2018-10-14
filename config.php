53da33a10ebe0d28e3974f0683d7d5c71093964d
ArhaamAcademy <35983296+ArhaamAcademy@users.noreply.github.com>
2018-10-14 15:34:04 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,21 @@
 
+<?php
+
+$dbhost = 'localhost';
+$dbname = 'db_name';
+$dbuser = 'root';
+$dbpass = '';
+
+try 
+{
+	
+	$db = new PDO("mysql:host={$dbhost};dbname={$dbname}",$dbuser,$dbpass);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch(PDOException $e)
+{
+	echo "Connection error".$e->getMessage();
+}
+
+?>
